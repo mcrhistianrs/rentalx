0841da90a84343578d8cfa4ce6eea13a40bbd0e3
Maicon Crhistian Ramos da Silva <maicon.s@efficienza.com.br>
2021-07-09 09:10:07 -0300

    Adicionado Category Service

--- src/services/CreateCategoryService.ts
+++ src/services/CreateCategoryService.ts
@@ -1,17 +1,21 @@
+import { ICategoriesRepository } from "../repositories/ICategoriesRepository";
+
 interface IRequest {
   name: string;
   description: string;
 }
 
 class CreateCategoryService {
-  execute({ description, name }: IRequest) {
-    const categoryAlreadyExists = categoriesRepository.findByName(name);
+  constructor(private categoriesRepository: ICategoriesRepository) {}
+
+  execute({ description, name }: IRequest): void {
+    const categoryAlreadyExists = this.categoriesRepository.findByName(name);
 
     if (categoryAlreadyExists) {
       throw new Error("Category already exists!");
     }
 
-    categoriesRepository.create({ name, description });
+    this.categoriesRepository.create({ name, description });
   }
 }
 
